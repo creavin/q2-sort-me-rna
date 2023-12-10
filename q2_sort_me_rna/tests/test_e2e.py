596140b3bd0e576887bf24676c1c866420a0ca65
creavin <thomas.creavin@gmail.com>
2023-12-10 17:36:35 +0100

    Tidy e2e test

--- q2_sort_me_rna/tests/test_e2e.py
+++ q2_sort_me_rna/tests/test_e2e.py
@@ -12,17 +12,17 @@ import os
 import tempfile
 import unittest
 
+
 class TestSortRNA(unittest.TestCase):
 
     def setUp(self):
         # TODO copy test assets to tmp dir
         self.q2smr_output_dir = tempfile.mkdtemp()
-        self.q2smr_output_artifacts_dir = f'{self.q2smr_output_dir}/qiime-output'
-
+        self.q2smr_output_artifacts_dir = \
+            f'{self.q2smr_output_dir}/qiime-output'
 
     def tearDown(self):
         shutil.rmtree(self.q2smr_output_dir)
-
 
     def test_sort_rna_end_to_end(self):
         command = [
@@ -41,17 +41,17 @@ class TestSortRNA(unittest.TestCase):
             "sam_aligned_seq.qza"
         ]
 
+        files = os.listdir(self.q2smr_output_artifacts_dir)
         for artifact in expected_artifacts:
-            self.assertIn(artifact, os.listdir(self.q2smr_output_artifacts_dir), "Expected file is not present")
+            self.assertIn(artifact, files, "Expected file is not present")
 
-
-        expected_meta_data = {
-            'Type': 'FeatureData[BLAST6]',
-            'format': 'BLAST6DirectoryFormat',
-        }
+        # expected_meta_data = {
+        #     'Type': 'FeatureData[BLAST6]',
+        #     'format': 'BLAST6DirectoryFormat',
+        # }
         # TODO re-impl
-        # self._validate_artifact_type(self.q2smr_output_artifact_path, expected_meta_data)
-
+        # self._validate_artifact_type(
+        # self.q2smr_output_artifact_path, expected_meta_data)
 
     def test_otu_mapping_end_to_end(self):
         command = [
@@ -75,30 +75,32 @@ class TestSortRNA(unittest.TestCase):
         ]
 
         for artifact in expected_artifacts:
-            self.assertIn(artifact, os.listdir(self.q2smr_output_artifacts_dir), "Expected file is not present")
+            files = os.listdir(self.q2smr_output_artifacts_dir)
+            self.assertIn(artifact, files, "Expected file is not present")
 
-
-        expected_meta_data = {
-            'Type': 'FeatureData[BLAST6]',
-            'format': 'BLAST6DirectoryFormat',
-        }
+        # expected_meta_data = {
+        #     'Type': 'FeatureData[BLAST6]',
+        #     'format': 'BLAST6DirectoryFormat',
+        # }
         # TODO re-impl
-        # self._validate_artifact_type(self.q2smr_output_artifact_path, expected_meta_data)
-
+        # self._validate_artifact_type(
+        # self.q2smr_output_artifact_path, expected_meta_data)
 
     def _validate_artifact_type(self, file, expected_meta_data):
         command = [
             'qiime', 'tools', 'peek', file
         ]
 
-        output = subprocess.run(command, check=True, stdout=subprocess.PIPE, text=True)
+        output = subprocess.run(command, check=True,
+                                stdout=subprocess.PIPE, text=True)
 
         key_value_pairs = re.findall(r'(\S+):\s+(\S+)', output.stdout)
         result_dict = dict(key_value_pairs)
 
+        err_msg = "Artifact metadata does not match the expected values"
         for key, value in expected_meta_data.items():
-            self.assertEqual(result_dict[key], value, 
-                             "Artifact metadata does not match the expected values")
+            self.assertEqual(result_dict[key], value, err_msg)
+
 
 if __name__ == '__main__':
     unittest.main()
