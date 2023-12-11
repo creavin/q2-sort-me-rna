544af8e77b4028809723c1dc5495627f4711e337
creavin <thomas.creavin@gmail.com>
2023-12-11 14:50:25 +0100

    Refactor rns sorter

--- q2_sort_me_rna/_rna_sorter.py
+++ q2_sort_me_rna/_rna_sorter.py
@@ -21,8 +21,8 @@ from q2_types_genomics.per_sample_data import SAMDirFmt
 def sort_rna(
         ref: str,
         reads: str,
+        workdir: str,
         # [COMMON]
-        workdir: str = 'out',
         kvdb: str = None,
         idx_dir: str = None,  # hyphenated
         readb: str = None,
@@ -83,31 +83,64 @@ def sort_rna(
     arg_value_dict = locals()
 
     command = 'sortmerna'
-    parameters = []
+    parameters = _parse_parameters(arg_value_dict)
+    command_string = f'{command} {" ".join(parameters)}'
 
+    _call_shell_command(command_string)
+
+    smr_output_dir = f'{workdir}/out'
+    smr_output_files = os.listdir(f'{smr_output_dir}')
+
+    for smr_file in smr_output_files:
+        extension = os.path.splitext(smr_file)[1]
+
+        if extension == '.blast':
+            blast_aligned_seq = _construct_blast_fmt(smr_output_dir, smr_file)
+        elif extension == '.fq':
+            fastx_aligned_seq = _construct_fastx_fmt(smr_output_dir, smr_file)
+        elif extension == '.sam':
+            sam_aligned_seq = _construct_sam_fmt(smr_output_dir, smr_file)
+        elif smr_file == 'otu_map.txt':
+            otu_mapping = _construct_otu_mapping(smr_output_dir, smr_file)
+
+    if 'otu_mapping' in locals():
+        return blast_aligned_seq, fastx_aligned_seq, \
+            sam_aligned_seq, otu_mapping
+    else:
+        return blast_aligned_seq, fastx_aligned_seq, sam_aligned_seq
+
+
+def _parse_parameters(arg_value_dict):
     uppercase_args = ['sq', 'f', 'n', 'r', 'l']
     hyphenated_args = ['idx_dir', 'no_best', 'zip_out', 'dbg_level']
     hard_coded_args = ['blast', 'fastx', 'sam']
+    parameters = []
 
+    # Assumption that the transformations are mutually exclusive
     for arg in arg_value_dict:
+        # Always set to true so all alignment types are produced
         if arg in hard_coded_args:
             arg_value_dict[arg] = 1
 
         if not (value := arg_value_dict[arg]) or arg == "arg_value_dict":
             continue
-
-        if arg in hyphenated_args:
-            arg = arg.replace('_', '-')  # e.g idx_dir -> idx-dir
-
-        if arg in uppercase_args:
-            arg = arg.upper()  # e.g sq -> SQ
-
-        if len(arg) == 1:  # single letter args use "-" e.g -e, -f, -h
+        elif arg in hyphenated_args:
+            # Some SortMe params must be hyphenated e.g idx_dir -> idx-dir
+            arg = arg.replace('_', '-')
+        elif arg in uppercase_args:
+            # Some SortMe params must be uppercase # e.g sq -> SQ
+            arg = arg.upper()
+        
+        # Single char args use "-" instead of "--" e.g -e, -f, -h
+        if len(arg) == 1:
             parameters.append(f'-{arg} {value}')
         else:
             parameters.append(f'--{arg} {value}')
 
-    command_string = f'{command} {" ".join(parameters)}'
+    return parameters
+
+
+def _call_shell_command(command_string):
     try:
         subprocess.run(command_string, shell=True, check=True)
     except subprocess.CalledProcessError as e:
@@ -117,57 +150,46 @@ def sort_rna(
         print(f"An error occurred: {e}")
         raise e
 
-    output_files = os.listdir(f'{workdir}/out')
 
-    for file in output_files:
-        extension = os.path.splitext(file)[1]
+def _construct_blast_fmt(smr_output_dir, file):
+    blast_fmt = BLAST6Format()
+    shutil.copy(f'{smr_output_dir}/{file}', f"{str(blast_fmt)}")
+    return blast_fmt
 
-        if extension == '.blast':
-            blast_fmt = BLAST6Format()
-            print(blast_fmt)
-            shutil.copy(f'{workdir}/out/aligned.blast', f"{str(blast_fmt)}")
-            print(blast_fmt.view(pd.DataFrame))
-            # return blast_fmt
-            blast_aligned_seq = blast_fmt
 
-        # TODO handle case where already gzipped
-        if extension == '.fq':
-            fastq_fmt = CasavaOneEightSingleLanePerSampleDirFmt()
+def _construct_fastx_fmt(smr_output_dir, file):
+    # TODO handle case where already gzipped
+    fastq_fmt = CasavaOneEightSingleLanePerSampleDirFmt()
 
-            _gzip_file(f'{workdir}/out/aligned.fq',
-                       f'{workdir}/out/aligned.fastq.gz')
-            shutil.copy(f'{workdir}/out/aligned.fastq.gz',
-                        f"{str(fastq_fmt)}/sample_name_L999_R1_001.fastq.gz")
+    _gzip_file(f'{smr_output_dir}/aligned.fq',
+               f'{smr_output_dir}/aligned.fastq.gz')
+    shutil.copy(f'{smr_output_dir}/aligned.fastq.gz',
+                f"{str(fastq_fmt)}/sample_name_L999_R1_001.fastq.gz")
 
-            print(f"fastq_fmt: {fastq_fmt}")
-            print(f"fastq_fmt manifest: \n{fastq_fmt.manifest}")
+    print(f"fastq_fmt: {fastq_fmt}")
+    print(f"fastq_fmt manifest: \n{fastq_fmt.manifest}")
 
-            fastx_aligned_seq = fastq_fmt
-            # return fastq_fmt
+    return fastq_fmt
 
-        if extension == '.sam':
-            sam_fmt = SAMDirFmt()
-            shutil.copy(f'{workdir}/out/aligned.sam', f"{str(sam_fmt)}")
 
-            sam_aligned_seq = sam_fmt
+def _construct_sam_fmt(smr_output_dir, file):
+    sam_fmt = SAMDirFmt()
+    shutil.copy(f'{smr_output_dir}/aligned.sam', f"{str(sam_fmt)}")
 
-        if file == 'otu_map.txt':
-            in_file = f'{workdir}/out/otu_map.txt'
-            out_file = f'{workdir}/out/processed_otu_map.txt'
+    return sam_fmt
 
-            awk_command = \
-                f"awk -F'\t' '{{print $1, NF-1}}' {in_file} > {out_file}"
-            subprocess.run(awk_command, shell=True)
-            df = pd.read_csv(out_file, index_col='Ref Sequence',
-                             sep=' ', names=['Ref Sequence', 'Count'])
 
-            otu_mapping = df
+def _construct_otu_mapping(smr_output_dir, file):
+    in_file = f'{smr_output_dir}/otu_map.txt'
+    out_file = f'{smr_output_dir}/processed_otu_map.txt'
 
-    if 'otu_mapping' in locals() and otu_mapping is not None:
-        return blast_aligned_seq, fastx_aligned_seq, \
-            sam_aligned_seq, otu_mapping
-    else:
-        return blast_aligned_seq, fastx_aligned_seq, sam_aligned_seq
+    awk_command = \
+        f"awk -F'\t' '{{print $1, NF-1}}' {in_file} > {out_file}"
+    subprocess.run(awk_command, shell=True)
+    mapping = pd.read_csv(out_file, index_col='Ref Sequence',
+                          sep=' ', names=['Ref Sequence', 'Count'])
+
+    return mapping
 
 
 def _gzip_file(input_file, output_file):
